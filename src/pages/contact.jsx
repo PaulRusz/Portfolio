57a50b4f4c31560302d5d7cad8356cf489d3f1f2
PaulRusz <paulruszkay@gmail.com>
2024-08-12 18:06:45 -0400

    removed handleSubmit from contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,43 +5,20 @@ import styles from "../styles/contact.module.scss";
 import linkedin from "../assets/images/linkedin.png";
 
 export default function Contact() {
-  // State for form data
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
 
-  // State for form submission status
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-
-  // Reference for the form
   const form = useRef();
 
-  // Handle form input changes
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (formData.name.trim() === "" || !formData.email.includes("@")) {
-      // Handle form validation errors
-      return;
-    }
-
-    // Form data is valid, proceed with submission
-    localStorage.setItem("formData", JSON.stringify(formData));
-    setIsFormSubmitted(true);
-
-    // Render the last submission to the page
-    console.log(formData);
-  };
-
-  // Send email with emailjs
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -55,35 +32,31 @@ export default function Contact() {
       .then(
         (response) => {
           console.log("SUCCESS!", response.status, response.text);
-          // Reset form fields after submission
           setFormData({
             name: "",
             email: "",
             message: "",
           });
 
-          // Clear "Message sent!" text after a delay
           setTimeout(() => {
             setIsFormSubmitted(false);
           }, 3000);
         },
         (error) => {
           console.error("FAILED...", error);
-          console.error("ERROR DETAILS", error.status, error.text);
         }
       );
   };
 
   return (
     <motion.div
-      className={styles.page1} // Use CSS Module
+      className={styles.page1}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 1 } }}
       exit={{ opacity: 0 }}
     >
       <div className={styles.contactContainer}>
         <h1>Contact Me</h1>
-
         <div className={styles.divider}></div>
 
         <div className={styles.contactTextContainer}>
