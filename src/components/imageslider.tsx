61fd197630a64501560f08c46a974ef2e6feb5d7
PaulRusz <paulruszkay@gmail.com>
2024-08-14 16:22:48 -0400

    changes to homepage & transition & added new photos

--- src/components/imageslider.tsx
+++ src/components/imageslider.tsx
@@ -1,98 +1,35 @@
 import React, { useState, useEffect } from "react";
 import image1 from "../assets/images/SA.png";
 import image2 from "../assets/images/SA2.png";
+import image3 from "../assets/images/Charleston.png";
+import image4 from "../assets/images/sophierizz.png";
 import styles from "../styles/imageslider.module.scss";
 
-// Remove `StaticImageData` interface usage if not needed
-interface ImageData {
-  src: string; // Use string since you're using the regular <img> tag
-}
+const images = [image1, image2, image3, image4];
 
-const images: ImageData[] = [
-  { src: image1 },
-  { src: image2 },
-  // { src: image3 },
-];
+export default function ImageSlider() {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export default function ImageSlider(): JSX.Element {
-  // Keeps track of the current image index
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-  // Determines if the image is being hovered over
-  const [isHovered, setIsHovered] = useState<boolean>(false);
-
-  // // Function to show the previous slide
-  // const prevSlide = (): void => {
-  //   setCurrentIndex(
-  //     (prevIndex) => (prevIndex - 1 + images.length) % images.length
-  //   );
-  // };
-
-  // Function to show the next slide
-  const nextSlide = (): void => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  // Effect to handle auto slide transition
   useEffect(() => {
-    // Start auto slide transition if not hovered
-    if (!isHovered) {
-      const interval = setInterval(() => {
-        nextSlide();
-      }, 3000);
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
 
-      // Cleans the interval on component unmount
-      return () => {
-        clearInterval(interval);
-      };
-    }
-  }, [isHovered]);
-
-  // Handle mouse over event
-  const handleMouseOver = (): void => {
-    setIsHovered(true);
-  };
-
-  // Handle mouse leave event
-  const handleMouseLeave = (): void => {
-    setIsHovered(false);
-  };
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className={styles.sliderContainer}>
-      <div
-        className={`${styles.sliderImage2} group hover:-translate-y-2`}
-        onMouseOver={handleMouseOver}
-        onMouseLeave={handleMouseLeave}
-      >
-        <img
-          className={styles.sliderImage}
-          src={images[currentIndex].src}
-          alt="" 
-        />
-      </div>
-      {/* <button
-          className="absolute left-0 top-1/2 transform h-[459px] rounded-xl hover:bg-[#1a222f] mx-1 -mt-[10px] -translate-y-1/2 bg-[#111927] text-white p-2 group"
-          onClick={prevSlide}
-        >
-          <ChevronLeft className="text-gray-400 group-hover:text-white" />
-        </button>
-        <button
-          className="absolute right-0 top-1/2 transform h-[459px] rounded-xl hover:bg-[#1a222f] mx-1 -mt-[10px] -translate-y-1/2 bg-[#111927] text-white p-2 group"
-          onClick={nextSlide}
-        >
-          <ChevronRight className="text-gray-400 group-hover:text-white" />
-        </button> */}
-      <div className="flex justify-center mt-4">
-        {images.map((_, index) => (
-          <div
+      <div className={styles.sliderImageWrapper}>
+        {images.map((src, index) => (
+          <img
             key={index}
-            className={`h-1 w-10 mx-1 ${
-              index === currentIndex
-                ? "bg-[#beff46] rounded-xl"
-                : "bg-gray-300 rounded-xl"
-            } transition-all duration-500 ease-in-out`}
-          ></div>
+            className={`${styles.sliderImage} ${
+              index === currentIndex ? styles.show : ""
+            }`}
+            src={src}
+            alt={`Slide ${index + 1}`}
+          />
         ))}
       </div>
     </div>
--- src/components/imageslider2.tsx
+++ src/components/imageslider2.tsx
@@ -1,88 +1,42 @@
 import React, { useState, useEffect } from "react";
-import image1 from "../assets/images/SA.png";
-import image2 from "../assets/images/SA2.png";
+import image1 from "../assets/images/boat.png";
+import image2 from "../assets/images/budapest.png";
+import image3 from "../assets/images/winterTrees.png";
+import image4 from "../assets/images/sophie.png";
 import styles from "../styles/imageslider2.module.scss";
 
-// Remove `StaticImageData` interface usage if not needed
-interface ImageData {
-  src: string; // Use string since you're using the regular <img> tag
-}
-
-const images: ImageData[] = [
-  { src: image1 },
-  { src: image2 },
-  // { src: image3 },
-];
+const images = [image1, image2, image3, image4];
 
 export default function ImageSlider2(): JSX.Element {
-  // Keeps track of the current image index
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  // Determines if the image is being hovered over
-  const [isHovered, setIsHovered] = useState<boolean>(false);
-
-  // Function to show the previous slide
-  // const prevSlide = (): void => {
-  //   setCurrentIndex(
-  //     (prevIndex) => (prevIndex - 1 + images.length) % images.length
-  //   );
-  // };
-
-  // Function to show the next slide
   const nextSlide = (): void => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  // Effect to handle auto slide transition
   useEffect(() => {
-    // Start auto slide transition if not hovered
-    if (!isHovered) {
-      const interval = setInterval(() => {
-        nextSlide();
-      }, 3000);
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000);
 
-      // Cleans the interval on component unmount
-      return () => {
-        clearInterval(interval);
-      };
-    }
-  }, [isHovered]);
-
-  // Handle mouse over event
-  const handleMouseOver = (): void => {
-    setIsHovered(true);
-  };
-
-  // Handle mouse leave event
-  const handleMouseLeave = (): void => {
-    setIsHovered(false);
-  };
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className={styles.sliderContainer2}>
-      <div
-        className={`${styles.sliderImage2} group hover:-translate-y-2`}
-        onMouseOver={handleMouseOver}
-        onMouseLeave={handleMouseLeave}
-      >
-        <img
-          className={styles.sliderImage2}
-          src={images[currentIndex].src}
-          alt="" 
-        />
+      <div className={styles.sliderImageWrapper2}>
+        {images.map((src, index) => (
+          <img
+            key={index}
+            className={`${styles.sliderImage2} ${
+              index === currentIndex ? styles.show : ""
+            }`}
+            src={src}
+            alt={`Slide ${index + 1}`}
+          />
+        ))}
       </div>
-      {/* <button
-          className="absolute left-0 top-1/2 transform h-[459px] rounded-xl hover:bg-[#1a222f] mx-1 -mt-[10px] -translate-y-1/2 bg-[#111927] text-white p-2 group"
-          onClick={prevSlide}
-        >
-          <ChevronLeft className="text-gray-400 group-hover:text-white" />
-        </button>
-        <button
-          className="absolute right-0 top-1/2 transform h-[459px] rounded-xl hover:bg-[#1a222f] mx-1 -mt-[10px] -translate-y-1/2 bg-[#111927] text-white p-2 group"
-          onClick={nextSlide}
-        >
-          <ChevronRight className="text-gray-400 group-hover:text-white" />
-        </button> */}
+
       <div className="flex justify-center mt-4">
         {images.map((_, index) => (
           <div
