9d867cf45ff3b5a77530f5a1ae20f59579969579
PaulRusz <paulruszkay@gmail.com>
2024-08-15 16:47:59 -0400

    attempt to make slider 2 load faster on homepage

--- src/components/imageslider2.tsx
+++ src/components/imageslider2.tsx
@@ -7,6 +7,7 @@ import styles from "../styles/imageslider2.module.scss";
 
 const images = [image1, image2, image3, image4];
 
+
 export default function ImageSlider2(): JSX.Element {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
@@ -15,10 +16,7 @@ export default function ImageSlider2(): JSX.Element {
   };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      nextSlide();
-    }, 3000);
-
+    const interval = setInterval(nextSlide, 3000);
     return () => clearInterval(interval);
   }, []);
 
