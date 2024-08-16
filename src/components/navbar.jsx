aa4b2388aca107c865784d6d99ee5b82bf4a8bb1
PaulRusz <paulruszkay@gmail.com>
2024-08-15 22:03:30 -0400

    changes to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../styles/navbar.module.scss";
+import { FaUser, FaBriefcase, FaEnvelope, FaFileAlt } from "react-icons/fa";
 
 export default function Navbar({ children }) {
   const [showMenu, setShowMenu] = useState(false);
@@ -26,22 +27,22 @@ export default function Navbar({ children }) {
         <ul className={`${styles.links} ${showMenu ? styles.showMenu : ""}`}>
           <li>
             <Link to="/about" className={styles.links}>
-              About
+              <FaUser /> About
             </Link>
           </li>
           <li>
             <Link to="/portfolio" className={styles.links}>
-              Portfolio
+              <FaBriefcase /> Portfolio
             </Link>
           </li>
           <li>
             <Link to="/contact" className={styles.links}>
-              Contact
+              <FaEnvelope /> Contact
             </Link>
           </li>
           <li>
             <Link to="/resume" className={styles.links}>
-              Resume
+              <FaFileAlt /> Resume
             </Link>
           </li>
         </ul>
