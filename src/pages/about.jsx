adb86b7f06cf34eafe031ed773bafbcfe26e7bcf
PaulRusz <paulruszkay@gmail.com>
2025-01-29 16:19:52 -0500

    changes to <p> on the about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -30,41 +30,47 @@ export default function About() {
           )}
 
           <div className={styles.narrativeContainer}>
-            <p>Born in New Jersey but call Delaware home.</p>
-            <div className={styles.divider3}></div>
-            <p>
-              My collegiate journey took me to Newark, Delaware, and Nashville,
-              Tennessee, where I earned a bachelor's degree in business.
-              However, my passion for technology has now led me down a different
-              path.
-            </p>
-            <p>
-              Over time, I embarked on a self-taught coding journey, delving
-              into the intricacies of programming. This newfound interest
-              inspired me to enroll in a full stack development course at
-              Columbia University.
-            </p>
-            <p>
-              While I acknowledge that there's always more to learn, I thrive on
-              the challenge of tackling complex problems and honing my skills.
-              I'm excited about the possibilities that lie ahead as I continue
-              to grow in this dynamic field.
-            </p>
-            <div className={styles.divider3}></div>
-            <p>
-              You can usually find me staying active, spending some downtime
-              outdoors, nerding out to Marvel, GoT, or most things sci-fi and
-              exploring this beautiful pale blue dot.
-            </p>
-            <p>
-              {/* I also spend my time diving into captivating reads, looking for
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, ease: "easeOut" }}
+            >
+              <p>Born in New Jersey but call Delaware home.</p>
+              <div className={styles.divider3}></div>
+              <p>
+                My collegiate journey took me to Newark, Delaware, and
+                Nashville, Tennessee, where I earned a bachelor's degree in
+                business. However, my passion for technology has now led me down
+                a different path.
+              </p>
+              <p>
+                Over time, I embarked on a self-taught coding journey, delving
+                into the intricacies of programming. This newfound interest
+                inspired me to enroll in a full stack development course at
+                Columbia University.
+              </p>
+              <p>
+                While I acknowledge that there's always more to learn, I thrive
+                on the challenge of tackling complex problems and honing my
+                skills. I'm excited about the possibilities that lie ahead as I
+                continue to grow in this dynamic field.
+              </p>
+              <div className={styles.divider3}></div>
+              <p>
+                You can usually find me staying active, spending some downtime
+                outdoors, nerding out to Marvel, GoT, or most things sci-fi and
+                exploring this beautiful pale blue dot.
+              </p>
+              <p>
+                {/* I also spend my time diving into captivating reads, looking for
               newly released music, or searching for the best lasagna the East
               Coast has to offer.
             </p>
             <p>
               I'm sure there's much more to me than what's listed here, so don't
               hesitate to reach out if you'd like to learn more. */}
-            </p>
+              </p>
+            </motion.p>
           </div>
         </div>
       </motion.div>
